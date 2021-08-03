b2e4a5c6cf4b203dcb1ed096e56bd8259d16c819
Course Test <course-test@cs.sfu.ca>
2021-08-03 21:26:40 +0000

    last commit, edited realloc, traded performance for simplicity

--- mm-realloc.c
+++ mm-realloc.c
@@ -12,13 +12,55 @@
 void *mm_realloc(void *ptr, size_t size)
 {
     // Write your code here ...
+
+    // this is my original code, it does not give performance as the memory is always rellocated, but this worked when I ran it first
+
     if(ptr == NULL && size > 0) 
-        mm_malloc(size);
-    else if(ptr != NULL && size == 0)
+        return mm_malloc(size);
+    else if(ptr != NULL && size == 0) 
+    {
         mm_free(ptr);
+        return NULL;
+    }
     
     BlockInfo * blockInfo = ((BlockInfo *) UNSCALED_POINTER_SUB(ptr, WORD_SIZE));
-    size_t oldSize = blockInfo->sizeAndTags;
+    size_t oldSize = SIZE(blockInfo->sizeAndTags);
+    
+    void * newBlock = mm_malloc(size);
+    if(oldSize > size)
+    {
+        for(int i = 0; i < size; i++)
+            ((char *) newBlock)[i] = ((char *) ptr)[i];
+        mm_free(ptr);
+        return newBlock;
+    }
+    
+    for(int i = 0; i < oldSize; i++)
+        ((char *) newBlock)[i] = ((char *) ptr)[i];
+    mm_free(ptr);
+    return newBlock;
+}
+
+
+    // this is my code that does not try to reallocate the block if it does not need to, however this one has kept on giving me segmentation fault
+    // I have tried fixing it in several ways, and have pushed it, but it does not work at all, hence I am pushing my older code that does not give
+    // performance, but overall, perf index is >= 90 so I guess it works well enough with the other cases where it reallocates when it does not need to.
+
+
+    /*if(ptr == NULL && size > 0) 
+        return mm_malloc(size);
+    else if(ptr != NULL && size == 0) 
+    {
+        mm_free(ptr);
+        return NULL;
+    }
+
+    //size += WORD_SIZE;
+    
+    BlockInfo * blockInfo = ((BlockInfo *) UNSCALED_POINTER_SUB(ptr, WORD_SIZE));
+    size_t oldSize = SIZE(blockInfo->sizeAndTags);
+
+    
     size_t precedingTagUsed = blockInfo->sizeAndTags & TAG_PRECEDING_USED;
     //size_t tagUsed = blockInfo->sizeAndTags & TAG_USED;
     
@@ -30,10 +72,24 @@ void *mm_realloc(void *ptr, size_t size)
     {
         if(size < MIN_BLOCK_SIZE) // new size should be at least the minblock size
             size = MIN_BLOCK_SIZE;
-        size_t freeSize = oldSize - size; // size of freeblock
-        blockInfo->sizeAndTags = size | precedingTagUsed; //current allocated block header updated
+        
+        size_t freeBlockSize = oldSize - size;
+        BlockInfo * ptrFreeBlock = UNSCALED_POINTER_ADD(blockInfo, size); //ptrAllBlock + reqSize;
+        ptrFreeBlock->sizeAndTags = freeBlockSize | TAG_PRECEDING_USED;
+        ((BlockInfo *)UNSCALED_POINTER_SUB(UNSCALED_POINTER_ADD(ptrFreeBlock, freeBlockSize), WORD_SIZE))->sizeAndTags = ptrFreeBlock->sizeAndTags;//(ptrFreeBlock + freeBlockSize - WORD_SIZE)->sizeAndTags = ptrFreeBlock->sizeAndTags;
+        BlockInfo* nextBlock = UNSCALED_POINTER_ADD(ptrFreeBlock, freeBlockSize);
+        nextBlock->sizeAndTags ^= TAG_PRECEDING_USED; // updating header of the next block to show previous block is now free
+        insertFreeBlock(ptrFreeBlock);
+        coalesceFreeBlock(ptrFreeBlock);
+        //examine_heap();
+        blockInfo->sizeAndTags = size | precedingTagUsed | TAG_USED;
+        
+        /*size_t freeSize = oldSize - size; // size of freeblock
+        blockInfo->sizeAndTags = size | precedingTagUsed | TAG_USED; //current allocated block header updated
         BlockInfo * freeBlock = UNSCALED_POINTER_ADD(blockInfo, size); // pointer to recently freed block
+
         freeBlock->sizeAndTags = freeSize | TAG_PRECEDING_USED; //setting header of free block
+        //mm_free(freeBlock);
         BlockInfo* nextBlock = UNSCALED_POINTER_ADD(freeBlock, freeSize);
         nextBlock->sizeAndTags ^= TAG_PRECEDING_USED; // updating header of the next block to show previous block is now free
         ((BlockInfo*)UNSCALED_POINTER_SUB(nextBlock, WORD_SIZE))->sizeAndTags = freeBlock->sizeAndTags; // updating footer of freeblock
@@ -43,11 +99,18 @@ void *mm_realloc(void *ptr, size_t size)
     }
     else 
     {
-        if(((((BlockInfo*) UNSCALED_POINTER_ADD(blockInfo, oldSize))->sizeAndTags) & TAG_USED) == 1) // if next block is not free call malloc, move everything over and free old block
+        /*void * newBlock = mm_malloc(size);
+        for(int i = 0; i < oldSize; i++)
+            ((char *) newBlock)[i] = ((char *) ptr)[i];
+        mm_free(ptr);
+        return newBlock;*/
+
+
+        /*if(((((BlockInfo*) UNSCALED_POINTER_ADD(blockInfo, oldSize))->sizeAndTags) & TAG_USED) == 1) // if next block is not free call malloc, move everything over and free old block
         {
-            BlockInfo* newBlock = mm_malloc(size);
-            for(int i = 1; i < oldSize; i++)
-                newBlock[i-1] = blockInfo[i];
+            void* newBlock = mm_malloc(size - WORD_SIZE);
+            for(int i = 0; i < oldSize; i++)
+                ((char *) newBlock)[i] = ((char *) ptr)[i];
             mm_free(blockInfo);
             return newBlock;
         }
@@ -56,16 +119,16 @@ void *mm_realloc(void *ptr, size_t size)
             BlockInfo* freeBlock = UNSCALED_POINTER_ADD(blockInfo, oldSize);
             if((SIZE(blockInfo->sizeAndTags) + SIZE(freeBlock->sizeAndTags)) < size) //the new size requested is more than the free and current sizes
             {
-                BlockInfo* newBlock = mm_malloc(size);
-                for(int i = 1; i < oldSize; i++)
-                    newBlock[i-1] = blockInfo[i];
+                void* newBlock = mm_malloc(size);
+                for(int i = 0; i < oldSize; i++)
+                    ((char *) newBlock)[i] = ((char *) ptr)[i];
                 mm_free(blockInfo);
                 return newBlock;
             }
             else // there is a free block and it is large enough to accomodate the new size constraint
             {
                 size_t totalSize = oldSize + SIZE(freeBlock->sizeAndTags);
-                blockInfo->sizeAndTags = size | precedingTagUsed;
+                blockInfo->sizeAndTags = size | precedingTagUsed | TAG_USED;
                 if(size == totalSize || (totalSize - size) < MIN_BLOCK_SIZE) //just large enough, no splitting required
                 {
                     BlockInfo* nextBlock = UNSCALED_POINTER_ADD(freeBlock, SIZE(freeBlock->sizeAndTags));
@@ -79,9 +142,10 @@ void *mm_realloc(void *ptr, size_t size)
                     splitBlock->sizeAndTags = (totalSize - size) | TAG_PRECEDING_USED;
                     ((BlockInfo*) (UNSCALED_POINTER_SUB(UNSCALED_POINTER_ADD(splitBlock, (totalSize-size)), WORD_SIZE)))->sizeAndTags = splitBlock->sizeAndTags; // update footer
                     // no need to update the tage preceeding used bit of next block since this was already a free block.
+                    removeFreeBlock(freeBlock);
                     insertFreeBlock(splitBlock);
                     coalesceFreeBlock(splitBlock);
-                    removeFreeBlock(freeBlock);
+                    
                     return UNSCALED_POINTER_ADD(blockInfo, WORD_SIZE);
                 }
                     
@@ -90,5 +154,7 @@ void *mm_realloc(void *ptr, size_t size)
     }
 
 
-    return NULL;
-}
+    //return NULL;  
+
+
+}*/
